04e8666266e7252471e831f11dd532d957ed3e42
Vladimir Chervanev <vchervanev@yandex.ru>
2015-04-15 22:02:11 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # img-debug-proxy
-Proxy in Go to help web-developers deal with images 
+Proxy in Go to help web-developers to deal with images 
+
+Current version is a proof of concept. Proxy handles popular image formats and adds a label "\<width\>x\<height\>" at its top-left corner.
+
+Planned features:
+
+* Parse a special image's URL parameter like "&xxx800x600" - expected dimentions. Check an actual image dimention and mark image correspondingly. 
+Generate empty image in case GET fails
+
+* web-admin UI, statistics, etc.
---
+++ img_debug_proxy.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+    "net/http"
+    "github.com/elazarl/goproxy"
+    "github.com/elazarl/goproxy/ext/image"
+    "fmt"
+    "log"
+    "flag"
+    "io/ioutil"
+    "image"
+    "golang.org/x/image/draw"
+    "code.google.com/p/freetype-go/freetype"
+    "code.google.com/p/freetype-go/freetype/truetype"
+    "code.google.com/p/freetype-go/freetype/raster"
+)
+
+var (
+    dpi      = flag.Float64("dpi", 72, "screen resolution in Dots Per Inch")
+    fontFile = flag.String("fontfile", "Lato-Regular.ttf", "filename of the ttf font")
+    hinting  = flag.String("hinting", "none", "none | full")
+    size     = flag.Float64("size", 12, "font size in points")
+    verbose  = flag.Bool("verbose", false, "proxy verbose mode, boolean")
+    port     = flag.String("port", "8080", "proxy port number")
+    indent   = flag.Int("indent", 5, "label indentation from the top-left image's corner in pixels")
+    fontContext  *freetype.Context
+)
+
+
+func main() {
+    flag.Parse()
+
+    font, err := loadFont()
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    fontContext = createContext(font)
+    fontHeight := int(fontContext.PointToFix32(*size)>>8)
+    // two points to output the text and its shadow
+    ptA := freetype.Pt(*indent + 1, *indent + 1 + fontHeight)
+    ptB := freetype.Pt(*indent, *indent + fontHeight)
+
+    proxy := goproxy.NewProxyHttpServer()
+    proxy.OnResponse().Do(goproxy_image.HandleImage(func(img image.Image, ctx *goproxy.ProxyCtx) image.Image {
+        outImage := image.NewRGBA(img.Bounds())
+        draw.Copy(outImage, image.ZP, img, img.Bounds(), nil)
+        text := fmt.Sprintf("%dx%d", img.Bounds().Dx(), img.Bounds().Dy())
+        fontContext.SetClip(img.Bounds())
+        fontContext.SetDst(outImage)
+
+        drawString(image.White, ptA, text)
+        drawString(image.Black, ptB, text)
+
+        return outImage
+    }))
+    proxy.Verbose = *verbose
+    log.Fatal(http.ListenAndServe(":" + *port, proxy))
+}
+
+func loadFont() (*truetype.Font, error){
+    fontBytes, err := ioutil.ReadFile(*fontFile)
+    if err != nil {
+        log.Println(err)
+        return nil, err
+    }
+    font, err := freetype.ParseFont(fontBytes)
+    if err != nil {
+        log.Println(err)
+        return nil, err
+    }
+    return font, nil
+}
+
+func drawString(src image.Image, position raster.Point, text string){
+    fontContext.SetSrc(src)
+    _, err := fontContext.DrawString(text, position)
+    if err != nil {
+        log.Println(err)
+    }
+}
+
+func createContext(font *truetype.Font) *freetype.Context {
+    ctx := freetype.NewContext()
+    ctx.SetDPI(*dpi)
+    ctx.SetFont(font)
+    ctx.SetFontSize(*size)
+    switch *hinting {
+        default:
+        ctx.SetHinting(freetype.NoHinting)
+        case "full":
+        ctx.SetHinting(freetype.FullHinting)
+    }
+    return ctx
+}
